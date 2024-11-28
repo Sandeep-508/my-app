701e8eefd2a6fc9edde50b2234690e6dfb958f15
Sandeep <elsenothing321@gmail.com>
2024-11-28 13:20:55 +0530

    added form layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,15 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        schoolbell: '"Schoolbell", serif',
+        luckiestGuy: '"Luckiest Guy", serif',
+        leagueSpartan: '"League Spartan", sans-serif',
+        Poppins: '"Poppins", sans-serif',
       },
+      boxShadow: {
+        box: "5px 3px 5px 5px #00000030",
+      },      
     },
   },
   plugins: [],
